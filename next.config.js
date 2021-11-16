e2b496ab5c90d07b38fc383172c584d766a15aec
Oleksandr Melnyk <alek.melnyk@gmail.com>
2021-11-16 18:24:00 +0200

    setup finished

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	env: {
+		MONGO_URI:
+			'mongodb+srv://Amereth:<8FGyGnzMf30cb6nI>@cluster0.yvso4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	},
 }
