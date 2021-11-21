522306e59a8bd17ac81b43fae332ecfeecfa31d7
Oleksandr Melnyk <alek.melnyk@gmail.com>
2021-11-21 16:04:02 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-	reactStrictMode: true,
-	env: {
-		MONGO_URI:
-			'mongodb+srv://Amereth:<8FGyGnzMf30cb6nI>@cluster0.yvso4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-	},
-}
